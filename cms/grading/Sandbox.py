c6788c9cd06ec4d75f4a369bc41803361dff3b58
muellerdo <domey038@live.de>
2020-05-20 18:02:22 +0200

    Added UTF-8 compatibility inside Isolate environments

--- cms/grading/Sandbox.py
+++ cms/grading/Sandbox.py
@@ -928,6 +928,12 @@ class IsolateSandbox(SandboxBase):
         # symlink to one out of many alternatives.
         self.maybe_add_mapped_directory("/etc/alternatives")
 
+        # MISITcms: Mount the /etc/ directory for Python Connections
+        self.maybe_add_mapped_directory("/etc/")
+        # MISITcms: Add the Python encoding environment variable for UTF-8
+        self.set_env["PYTHONIOENCODING"] = "utf8"
+        self.inherit_env.append("LC_ALL")
+
         # Tell isolate to get the sandbox ready. We do our best to cleanup
         # after ourselves, but we might have missed something if a previous
         # worker was interrupted in the middle of an execution, so we issue an
