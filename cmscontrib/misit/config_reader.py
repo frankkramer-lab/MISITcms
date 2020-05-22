37cc139e4964d02be13b681224d82a184fc2af02
muellerdo <domey038@live.de>
2020-05-22 10:48:31 +0200

    Added a cms.conf reader for fix config extraction inside and outside of CMS

---
+++ cmscontrib/misit/config_reader.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python3
+
+# Contest Management System - http://cms-dev.github.io/
+# Copyright © 2015-2016 Stefano Maggiolo <s.maggiolo@gmail.com>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+"""This script allow easy parsing the CMS config file in JSON format.
+"""
+
+import os
+import sys
+import json
+
+# Parse the config file and obtain the value of a given key
+def obtain_value(key, config_path=os.path.join("config", "cms.conf")):
+    # Transform relative to absolute path
+    config_path_abs = os.path.abspath(config_path)
+    # Read JSON config file as dictionary
+    with open(config_path_abs, "r", encoding="utf-8") as f:
+        conf_data = json.load(f)
+    # Return value for given key
+    return conf_data[key]
+
+# Provide an interface for direct script calling
+if __name__ == '__main__':
+    # IF script is called with one argument (key)
+    if len(sys.argv) == 2:
+        # Get value for given key from config file
+        value = obtain_value(sys.argv[1])
+        print(value)
+    else:
+        # Unknown parameter / Wrong call
+        # Do nothing
+        pass
